23eb4e620085865f1f8af092ab5a48a971d90b24
Guilherme Proença <cravo-guilherme1@hotmail.com>
2021-05-08 10:17:55 -0300

    Fixing SaleController

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -28,7 +28,7 @@ public class SaleController {
 		return ResponseEntity.ok(list);
 	}
 	
-	@GetMapping(value = "/sum-by-seller")
+	@GetMapping(value = "/amount-by-seller")
 	public ResponseEntity<List<SaleSumDTO>> amountGroupBySeller() {
 		List<SaleSumDTO> list = service.amountGroupBySeller();
 		return ResponseEntity.ok(list);
